bc2ad05bb8e7e00d4a158d228b02ffbc3cd36b67
Wing <winghangsong@gmail.com>
2023-03-08 21:03:01 -0500

    React-Rainbow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
+
 import './App.css'
 
 function App() {
-  let colors = [
+  let [colors, setColors] = useState ([
     'violet','blue', 
     'lightblue', 'green', 
     'greenyellow', 'yellow', 
     'orange', 'red'
-  ]
+])
 
 let colorMap = colors.map((color, i) => {
   return (
@@ -16,11 +18,17 @@ let colorMap = colors.map((color, i) => {
   )
 })
 
+
+const addColor = (newColor) => {
+  setColors([...colors, newColor])
+}
+
   return (
     <div className="App">
-        {colorMap}
+      {colorMap}
+      <ColorForm addColor={addColor} />
     </div>
-  );
+  )
 }
 
 export default App;
